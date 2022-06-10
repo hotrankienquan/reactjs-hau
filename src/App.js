13dafc542c487ddf638af9623f704a245339bcab
Kienquan <hotrankienquan144@gmail.com>
2022-06-10 11:19:32 +0700

    add video giac mo co that

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Learn react with quan</h1>
+        <h1>Giấc mơ có thật</h1>
+        <iframe
+          width="900"
+          height="700"
+          src="https://www.youtube.com/embed/MERX5z6RrSs?list=RDMERX5z6RrSs"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </header>
     </div>
   );
